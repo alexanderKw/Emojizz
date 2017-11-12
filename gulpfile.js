d6f3afbb5ce35a30bf047f72ebf1b4d250a64399
alexanderKw <alkrivulya@gmail.com>
2017-11-13 01:19:55 +0200

     added gulp.spritesmith

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,16 @@ const htmlmin = require('gulp-htmlmin');
 const postcss = require('gulp-postcss');
 const cssnext = require('postcss-cssnext');
 const cssnano = require('cssnano');
+const concat = require('gulp-concat');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
+const spritesmith = require('gulp.spritesmith');
+const buffer = require('vinyl-buffer');
+const imagemin = require('gulp-imagemin');
+const merge = require('merge-stream');
 const bs = require('browser-sync').create();
 const del = require('del');
 
@@ -54,7 +59,7 @@ gulp.task('css', () => {
       })
     )
     .pipe(postcss(processors))
-    .pipe(rename('styles.min.css'))
+    .pipe(concat('all.min.css'))
     .pipe(gulp.dest('./dist/css/'))
     .pipe(bs.stream());
 });
@@ -81,6 +86,26 @@ gulp.task('js', () => {
     .pipe(rename('main.min.js'))
     .pipe(gulp.dest('./dist/js/'))
     .pipe(bs.stream());
+});
+
+gulp.task('sprite', () => {
+  let spriteData = gulp.src('./app/img/images/*.png').pipe(
+    spritesmith({
+      imgName: 'sprite.png',
+      cssName: 'sprite.css',
+      padding: 20,
+      imgPath: '../img/sprites/sprite.png'
+    })
+  );
+
+  let imgStream = spriteData.img
+    .pipe(buffer())
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/img/sprites/'));
+
+  let cssStream = spriteData.css.pipe(gulp.dest('./app/css/'));
+
+  return merge(imgStream, cssStream);
 });
 
 gulp.task('emoji-json', () => {
